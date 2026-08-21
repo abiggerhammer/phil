47228645bcb78ac90f881c7222369b5c9e8adc3e
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-21 16:41:28 +0200

    Add Phil Core syntax types

---
+++ src/Phil/Core/Syntax.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Phil.Core.Syntax
+  ( Name (..)
+  , Mode (..)
+  , Ty (..)
+  , Proposition (..)
+  , Outcome (..)
+  , Session (..)
+  , Control (..)
+  , ObligationId (..)
+  , Obligation (..)
+  ) where
+
+import Data.Text (Text)
+
+newtype Name = Name { unName :: Text }
+  deriving (Eq, Ord, Show)
+
+data Mode
+  = Unrestricted
+  | Affine
+  | Linear
+  deriving (Eq, Ord, Show)
+
+data Ty
+  = TyUnit
+  | TyBool
+  | TyUInt Int
+  | TyBytes Text
+  | TyProof Proposition
+  | TyEndpoint Session
+  | TyRefined Name Ty Proposition
+  | TyOpaque Text
+  deriving (Eq, Ord, Show)
+
+data Proposition
+  = Truth
+  | Atom Text [Text]
+  | Equal Text Text
+  | Conjunction Proposition Proposition
+  deriving (Eq, Ord, Show)
+
+newtype Outcome = Outcome { unOutcome :: Text }
+  deriving (Eq, Ord, Show)
+
+data Session
+  = Send Name Ty Session
+  | Receive Name Ty Session
+  | Select [(Text, Session)]
+  | Offer [(Text, Session)]
+  | End Outcome
+  | Rec Name Session
+  | SessionVar Name
+  deriving (Eq, Ord, Show)
+
+data Control
+  = Continue
+  | Return Ty
+  | Closed Outcome
+  | Failed Text Text
+  deriving (Eq, Ord, Show)
+
+newtype ObligationId = ObligationId { unObligationId :: Text }
+  deriving (Eq, Ord, Show)
+
+data Obligation = Obligation
+  { obligationId :: ObligationId
+  , obligationProposition :: Proposition
+  , obligationOrigin :: Text
+  , obligationRequiredPoint :: Text
+  }
+  deriving (Eq, Ord, Show)
