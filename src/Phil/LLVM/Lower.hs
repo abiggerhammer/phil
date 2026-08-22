5f485c3275a86be81c547eff6d03dcb3701bf908
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 11:01:38 +0200

    Lower recognized records through ABI v1

--- src/Phil/LLVM/Lower.hs
+++ src/Phil/LLVM/Lower.hs
@@ -2,6 +2,7 @@
 
 module Phil.LLVM.Lower
   ( lowerSystemsConservative
+  , lowerSystemsRecognizedRecord
   ) where
 
 import qualified Data.Map.Strict as Map
@@ -10,8 +11,23 @@ import qualified Data.Text as Text
 import Phil.LLVM.IR
 import Phil.Systems.IR
 
+data LoweringMode
+  = ConservativeMode
+  | RecognizedRecordMode
+  deriving (Eq, Show)
+
 lowerSystemsConservative :: LLVMTargetProfile -> SystemsArtifact -> LLVMArtifact
-lowerSystemsConservative target systemsArtifact = artifact
+lowerSystemsConservative = lowerSystemsWith ConservativeMode
+
+lowerSystemsRecognizedRecord :: LLVMTargetProfile -> SystemsArtifact -> LLVMArtifact
+lowerSystemsRecognizedRecord = lowerSystemsWith RecognizedRecordMode
+
+lowerSystemsWith
+  :: LoweringMode
+  -> LLVMTargetProfile
+  -> SystemsArtifact
+  -> LLVMArtifact
+lowerSystemsWith mode target systemsArtifact = artifact
   where
     program = systemsArtifactProgram systemsArtifact
     sourceContract = systemsArtifactStageContract systemsArtifact
@@ -25,7 +41,7 @@ lowerSystemsConservative target systemsArtifact = artifact
       , llvmRuntimeABIProfile = llvmTargetRuntimeABIProfile target
       , llvmCompilationProfile = systemsProgramProfile program
       , llvmFunctions = Map.fromList
-          [ (functionKey, lowerFunction functionValue)
+          [ (functionKey, lowerFunction mode functionValue)
           | (functionKey, functionValue) <- Map.toAscList (systemsProgramFunctions program)
           ]
       , llvmStrengthenings = Map.empty
@@ -43,25 +59,25 @@ lowerSystemsConservative target systemsArtifact = artifact
       , llvmArtifactContract = contract
       }
 
-lowerFunction :: SystemsFunction -> LLVMFunction
-lowerFunction functionValue = LLVMFunction
+lowerFunction :: LoweringMode -> SystemsFunction -> LLVMFunction
+lowerFunction mode functionValue = LLVMFunction
   { llvmFunctionName = systemsFunctionName functionValue
   , llvmFunctionEntry = lowerBlockId (systemsFunctionEntry functionValue)
   , llvmFunctionBlocks = Map.fromList
-      [ (lowerBlockId blockKey, lowerBlock functionValue blockValue)
+      [ (lowerBlockId blockKey, lowerBlock mode functionValue blockValue)
       | (blockKey, blockValue) <- Map.toAscList (systemsFunctionBlocks functionValue)
       ]
   }
 
-lowerBlock :: SystemsFunction -> SystemsBlock -> LLVMBlock
-lowerBlock functionValue blockValue = LLVMBlock
+lowerBlock :: LoweringMode -> SystemsFunction -> SystemsBlock -> LLVMBlock
+lowerBlock mode functionValue blockValue = LLVMBlock
   { llvmBlockId = lowerBlockId (systemsBlockId blockValue)
-  , llvmBlockOps = concatMap lowerOp (systemsBlockOps blockValue)
-  , llvmBlockTerminator = lowerTerminator functionValue (systemsBlockTerminator blockValue)
+  , llvmBlockOps = concatMap (lowerOp mode functionValue) (systemsBlockOps blockValue)
+  , llvmBlockTerminator = lowerTerminator mode functionValue (systemsBlockTerminator blockValue)
   }
 
-lowerOp :: SystemsOp -> [LLVMOp]
-lowerOp operation = case operation of
+lowerOp :: LoweringMode -> SystemsFunction -> SystemsOp -> [LLVMOp]
+lowerOp mode functionValue operation = case operation of
   OpReceiveFrame { receiveGrammar = grammar } ->
     [LLVMCall ("receive_frame." <> grammar)]
   OpBorrowView {} -> [LLVMPlain "borrowed view; no representation copy"]
@@ -69,10 +85,20 @@ lowerOp operation = case operation of
   OpDestroyPending {} -> [LLVMCleanup "destroy pending ingress/frame"]
   OpReleaseOwner {} -> [LLVMCleanup "release owner"]
   OpCleanupPartial {} -> [LLVMCleanup "cleanup partial owner"]
-  OpRuntimeCall { runtimeCallName = name, runtimeCallSite = maybeSite } ->
-    case maybeSite of
-      Nothing -> [LLVMCall name]
-      Just site -> [LLVMRuntime site name]
+  OpRuntimeCall
+    { runtimeCallName = name
+    , runtimeCallInputs = inputs
+    , runtimeCallOutputs = outputs
+    , runtimeCallSite = maybeSite
+    } ->
+      case maybeSite of
+        Just site -> [LLVMRuntime site name]
+        Nothing -> case mode of
+          RecognizedRecordMode
+            | isRecordMaterialization functionValue name inputs outputs -> []
+            | Just projection <- recordProjection functionValue name inputs outputs ->
+                [projection]
+          _ -> [LLVMCall name]
   OpCopy {} -> [LLVMCall "copy"]
   OpEraseFact {} -> [LLVMMetadata "proof/typestate fact erased before LLVM"]
   OpDiagnostic { diagnosticName = name } -> [LLVMMetadata ("diagnostic " <> name)]
@@ -80,16 +106,48 @@ lowerOp operation = case operation of
     [LLVMScalarLiteral (unValueId output) literal]
   OpTraceEvent name -> [LLVMMetadata ("trace " <> name)]
 
-lowerTerminator :: SystemsFunction -> SystemsTerminator -> LLVMTerminator
-lowerTerminator functionValue terminator = case terminator of
+lowerTerminator :: LoweringMode -> SystemsFunction -> SystemsTerminator -> LLVMTerminator
+lowerTerminator mode functionValue terminator = case terminator of
   TermJump target -> LLVMJump (lowerBlockId target)
   TermBranch _ yes no -> LLVMBranch (lowerBlockId yes) (lowerBlockId no)
-  TermRecognize { recognizeSite = site, recognizeSuccess = yes, recognizeFailure = no } ->
-    runtimeBranch site yes no
+  TermRecognize
+    { recognizePending = pending
+    , recognizeSite = site
+    , recognizeSuccess = yes
+    , recognizeFailure = no
+    } ->
+      case mode of
+        RecognizedRecordMode ->
+          case recognizedRecordForSuccess functionValue pending yes of
+            Just (grammar, recordValue) ->
+              LLVMRecognizeRecord
+                site
+                grammar
+                (unValueId recordValue)
+                (lowerBlockId yes)
+                (lowerBlockId no)
+            Nothing -> runtimeBranch site yes no
+        ConservativeMode -> runtimeBranch site yes no
   TermRuntimeCheck { checkSite = site, checkSuccess = yes, checkFailure = no } ->
     runtimeBranch site yes no
-  TermReceiveExact { exactSite = site, exactSuccess = yes, exactFailure = no } ->
-    runtimeBranch site yes no
+  TermReceiveExact
+    { exactLength = lengthValue
+    , exactSite = site
+    , exactSuccess = yes
+    , exactFailure = no
+    } ->
+      case mode of
+        RecognizedRecordMode ->
+          case scalarTypeOf functionValue lengthValue of
+            Just scalarType -> LLVMRuntimeScalarBranch
+              site
+              ("receive_exact_" <> scalarSuffix scalarType)
+              (unValueId lengthValue)
+              scalarType
+              (lowerBlockId yes)
+              (lowerBlockId no)
+            Nothing -> runtimeBranch site yes no
+        ConservativeMode -> runtimeBranch site yes no
   TermSendExact { sendExactSite = site, sendExactSuccess = yes, sendExactFailure = no } ->
     runtimeBranch site yes no
   TermStore { storeSite = site, storeSuccess = yes, storeFailure = no } ->
@@ -105,6 +163,79 @@ lowerTerminator functionValue terminator = case terminator of
 runtimeBranch :: RuntimeSiteRef -> BlockId -> BlockId -> LLVMTerminator
 runtimeBranch site yes no =
   LLVMRuntimeBranch site (siteName site) (lowerBlockId yes) (lowerBlockId no)
+
+recognizedRecordForSuccess
+  :: SystemsFunction
+  -> ValueId
+  -> BlockId
+  -> Maybe (Text, ValueId)
+recognizedRecordForSuccess functionValue _pending successBlock = do
+  blockValue <- Map.lookup successBlock (systemsFunctionBlocks functionValue)
+  firstRecognizedRecord functionValue (systemsBlockOps blockValue)
+
+firstRecognizedRecord :: SystemsFunction -> [SystemsOp] -> Maybe (Text, ValueId)
+firstRecognizedRecord _ [] = Nothing
+firstRecognizedRecord functionValue (operation : rest) =
+  case operation of
+    OpRuntimeCall
+      { runtimeCallName = name
+      , runtimeCallInputs = []
+      , runtimeCallOutputs = [output]
+      , runtimeCallSite = Nothing
+      } ->
+        case Map.lookup output (systemsFunctionValues functionValue) of
+          Just SystemsValue { systemsValueRole = RuntimeRecord grammar }
+            | name == "materialize recognized " <> grammar -> Just (grammar, output)
+          _ -> firstRecognizedRecord functionValue rest
+    _ -> firstRecognizedRecord functionValue rest
+
+isRecordMaterialization
+  :: SystemsFunction
+  -> Text
+  -> [ValueId]
+  -> [ValueId]
+  -> Bool
+isRecordMaterialization functionValue name inputs outputs =
+  case (inputs, outputs) of
+    ([], [output]) ->
+      case Map.lookup output (systemsFunctionValues functionValue) of
+        Just SystemsValue { systemsValueRole = RuntimeRecord grammar } ->
+          name == "materialize recognized " <> grammar
+        _ -> False
+    _ -> False
+
+recordProjection
+  :: SystemsFunction
+  -> Text
+  -> [ValueId]
+  -> [ValueId]
+  -> Maybe LLVMOp
+recordProjection functionValue name inputs outputs =
+  case (inputs, outputs) of
+    ([recordValue], [outputValue]) -> do
+      SystemsValue { systemsValueRole = RuntimeRecord grammar } <-
+        Map.lookup recordValue (systemsFunctionValues functionValue)
+      SystemsValue { systemsValueRole = TypedScalar scalarType } <-
+        Map.lookup outputValue (systemsFunctionValues functionValue)
+      fieldName <- Text.stripPrefix ("project recognized " <> grammar <> ".") name
+      pure (LLVMFieldProjection
+        (unValueId outputValue)
+        (unValueId recordValue)
+        grammar
+        fieldName
+        scalarType)
+    _ -> Nothing
+
+scalarTypeOf :: SystemsFunction -> ValueId -> Maybe ScalarType
+scalarTypeOf functionValue valueId = do
+  SystemsValue { systemsValueRole = TypedScalar scalarType } <-
+    Map.lookup valueId (systemsFunctionValues functionValue)
+  pure scalarType
+
+scalarSuffix :: ScalarType -> Text
+scalarSuffix scalarType = case scalarType of
+  ScalarBool -> "bool"
+  ScalarUInt width -> "u" <> Text.pack (show width)
 
 siteName :: RuntimeSiteRef -> Text
 siteName site = Text.pack (show (runtimeSiteKind site))
