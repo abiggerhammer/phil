3e709740b3050668ab58d7b50856434105f71ca0
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 09:17:00 +0200

    Gate Phase 0 LLVM emission on certification

--- app/LLVMPhase0Main.hs
+++ app/LLVMPhase0Main.hs
@@ -1,7 +1,18 @@
 module Main (main) where
 
 import qualified Data.Text.IO as TextIO
-import Phil.LLVM (llvmArtifactText, phase0LLVMArtifact)
+import Phil.LLVM
+  ( llvmArtifactText
+  , phase0LLVMArtifact
+  , verifyPhase0LLVMCertification
+  )
+import System.Exit (exitFailure)
+import qualified System.IO as IO
 
 main :: IO ()
-main = TextIO.putStr (llvmArtifactText phase0LLVMArtifact)
+main = case verifyPhase0LLVMCertification of
+  Left certificationError -> do
+    IO.hPutStrLn IO.stderr $
+      "Phase 0 LLVM translation certification failed: " <> show certificationError
+    exitFailure
+  Right () -> TextIO.putStr (llvmArtifactText phase0LLVMArtifact)
