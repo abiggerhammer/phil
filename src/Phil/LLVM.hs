850374d4452e753c9b37505e64764bdad9ca43f6
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 19:02:28 +0200

    Resolve digest exports with exact-receive proof tranche

--- src/Phil/LLVM.hs
+++ src/Phil/LLVM.hs
@@ -8,6 +8,7 @@ module Phil.LLVM
   , module Phil.LLVM.RecognizedRecordProofCertification
   , module Phil.LLVM.ExactReceive
   , module Phil.LLVM.ExactReceiveCertification
+  , module Phil.LLVM.ExactReceiveProofCertification
   , module Phil.LLVM.DigestValidation
   , module Phil.LLVM.DigestValidationCertification
   ) where
@@ -16,6 +17,7 @@ import Phil.LLVM.DigestValidation
 import Phil.LLVM.DigestValidationCertification
 import Phil.LLVM.ExactReceive
 import Phil.LLVM.ExactReceiveCertification
+import Phil.LLVM.ExactReceiveProofCertification
 import Phil.LLVM.IR
 import Phil.LLVM.Lower
 import Phil.LLVM.Phase0
