efbe43c7ff22ed458667effb5c3fae6ec05aac1b
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 07:01:02 +0200

    Expose systems representation API

---
+++ src/Phil/Systems.hs
@@ -0,0 +1,9 @@
+module Phil.Systems
+  ( module Phil.Systems.IR
+  , module Phil.Systems.Verify
+  , module Phil.Systems.Phase0
+  ) where
+
+import Phil.Systems.IR
+import Phil.Systems.Phase0
+import Phil.Systems.Verify
