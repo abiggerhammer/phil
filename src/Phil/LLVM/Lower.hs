350b20c1ba6214b0c4b60dad7473def9dc47200e
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 11:03:24 +0200

    Lower recognized records through ABI v1 primitives

--- src/Phil/LLVM/Lower.hs
+++ src/Phil/LLVM/Lower.hs
@@ -25,7 +25,7 @@ lowerSystemsConservative target systemsArtifact = artifact
       , llvmRuntimeABIProfile = llvmTargetRuntimeABIProfile target
       , llvmCompilationProfile = systemsProgramProfile program
       , llvmFunctions = Map.fromList
-          [ (functionKey, lowerFunction functionValue)
+          [ (functionKey, lowerFunction target functionValue)
           | (functionKey, functionValue) <- Map.toAscList (systemsProgramFunctions program)
           ]
       , llvmStrengthenings = Map.empty
@@ -43,25 +43,25 @@ lowerSystemsConservative target systemsArtifact = artifact
       , llvmArtifactContract = contract
       }
 
-lowerFunction :: SystemsFunction -> LLVMFunction
-lowerFunction functionValue = LLVMFunction
+lowerFunction :: LLVMTargetProfile -> SystemsFunction -> LLVMFunction
+lowerFunction target functionValue = LLVMFunction
   { llvmFunctionName = systemsFunctionName functionValue
   , llvmFunctionEntry = lowerBlockId (systemsFunctionEntry functionValue)
   , llvmFunctionBlocks = Map.fromList
-      [ (lowerBlockId blockKey, lowerBlock functionValue blockValue)
+      [ (lowerBlockId blockKey, lowerBlock target functionValue blockValue)
       | (blockKey, blockValue) <- Map.toAscList (systemsFunctionBlocks functionValue)
       ]
   }
 
-lowerBlock :: SystemsFunction -> SystemsBlock -> LLVMBlock
-lowerBlock functionValue blockValue = LLVMBlock
+lowerBlock :: LLVMTargetProfile -> SystemsFunction -> SystemsBlock -> LLVMBlock
+lowerBlock target functionValue blockValue = LLVMBlock
   { llvmBlockId = lowerBlockId (systemsBlockId blockValue)
-  , llvmBlockOps = concatMap lowerOp (systemsBlockOps blockValue)
-  , llvmBlockTerminator = lowerTerminator functionValue (systemsBlockTerminator blockValue)
+  , llvmBlockOps = concatMap (lowerOp target functionValue) (systemsBlockOps blockValue)
+  , llvmBlockTerminator = lowerTerminator target functionValue (systemsBlockTerminator blockValue)
   }
 
-lowerOp :: SystemsOp -> [LLVMOp]
-lowerOp operation = case operation of
+lowerOp :: LLVMTargetProfile -> SystemsFunction -> SystemsOp -> [LLVMOp]
+lowerOp target functionValue operation = case operation of
   OpReceiveFrame { receiveGrammar = grammar } ->
     [LLVMCall ("receive_frame." <> grammar)]
   OpBorrowView {} -> [LLVMPlain "borrowed view; no representation copy"]
@@ -69,10 +69,17 @@ lowerOp operation = case operation of
   OpDestroyPending {} -> [LLVMCleanup "destroy pending ingress/frame"]
   OpReleaseOwner {} -> [LLVMCleanup "release owner"]
   OpCleanupPartial {} -> [LLVMCleanup "cleanup partial owner"]
-  OpRuntimeCall { runtimeCallName = name, runtimeCallSite = maybeSite } ->
-    case maybeSite of
-      Nothing -> [LLVMCall name]
-      Just site -> [LLVMRuntime site name]
+  OpRuntimeCall
+    { runtimeCallName = name
+    , runtimeCallInputs = inputs
+    , runtimeCallOutputs = outputs
+    , runtimeCallSite = maybeSite
+    } ->
+      case maybeSite of
+        Just site -> [LLVMRuntime site name]
+        Nothing -> case abiV1FieldProjection target functionValue name inputs outputs of
+          Just encoded -> [LLVMCall encoded]
+          Nothing -> [LLVMCall name]
   OpCopy {} -> [LLVMCall "copy"]
   OpEraseFact {} -> [LLVMMetadata "proof/typestate fact erased before LLVM"]
   OpDiagnostic { diagnosticName = name } -> [LLVMMetadata ("diagnostic " <> name)]
@@ -80,16 +87,38 @@ lowerOp operation = case operation of
     [LLVMScalarLiteral (unValueId output) literal]
   OpTraceEvent name -> [LLVMMetadata ("trace " <> name)]
 
-lowerTerminator :: SystemsFunction -> SystemsTerminator -> LLVMTerminator
-lowerTerminator functionValue terminator = case terminator of
-  TermJump target -> LLVMJump (lowerBlockId target)
+lowerTerminator :: LLVMTargetProfile -> SystemsFunction -> SystemsTerminator -> LLVMTerminator
+lowerTerminator target functionValue terminator = case terminator of
+  TermJump targetBlock -> LLVMJump (lowerBlockId targetBlock)
   TermBranch _ yes no -> LLVMBranch (lowerBlockId yes) (lowerBlockId no)
-  TermRecognize { recognizeSite = site, recognizeSuccess = yes, recognizeFailure = no } ->
-    runtimeBranch site yes no
+  TermRecognize
+    { recognizePending = pending
+    , recognizeSite = site
+    , recognizeSuccess = yes
+    , recognizeFailure = no
+    } ->
+      if isRecognizedRecordV1 target
+        then LLVMRuntimeBranch
+          site
+          (recognitionRuntimeName functionValue pending site)
+          (lowerBlockId yes)
+          (lowerBlockId no)
+        else runtimeBranch site yes no
   TermRuntimeCheck { checkSite = site, checkSuccess = yes, checkFailure = no } ->
     runtimeBranch site yes no
-  TermReceiveExact { exactSite = site, exactSuccess = yes, exactFailure = no } ->
-    runtimeBranch site yes no
+  TermReceiveExact
+    { exactLength = lengthValue
+    , exactSite = site
+    , exactSuccess = yes
+    , exactFailure = no
+    } ->
+      if isRecognizedRecordV1 target && isU64Value functionValue lengthValue
+        then LLVMRuntimeBranch
+          site
+          ("abi-v1:receive-exact-u64:" <> unValueId lengthValue)
+          (lowerBlockId yes)
+          (lowerBlockId no)
+        else runtimeBranch site yes no
   TermSendExact { sendExactSite = site, sendExactSuccess = yes, sendExactFailure = no } ->
     runtimeBranch site yes no
   TermStore { storeSite = site, storeSuccess = yes, storeFailure = no } ->
@@ -101,6 +130,76 @@ lowerTerminator functionValue terminator = case terminator of
       _ -> LLVMReturn ("invalid-scalar-return:" <> unValueId valueId)
   TermEnd outcome -> LLVMReturn outcome
   TermFatal failure -> LLVMReturn ("fatal:" <> failure)
+
+abiV1FieldProjection
+  :: LLVMTargetProfile
+  -> SystemsFunction
+  -> Text
+  -> [ValueId]
+  -> [ValueId]
+  -> Maybe Text
+abiV1FieldProjection target functionValue name inputs outputs
+  | not (isRecognizedRecordV1 target) = Nothing
+  | otherwise = do
+      semanticField <- Text.stripPrefix "project recognized " name
+      (grammar, fieldName) <- splitSemanticField semanticField
+      recordValue <- case inputs of
+        [single] -> Just single
+        _ -> Nothing
+      outputValue <- case outputs of
+        [single] -> Just single
+        _ -> Nothing
+      recordRole <- systemsValueRole <$> Map.lookup recordValue (systemsFunctionValues functionValue)
+      scalarRole <- systemsValueRole <$> Map.lookup outputValue (systemsFunctionValues functionValue)
+      case (recordRole, scalarRole) of
+        (RuntimeRecord recordGrammar, TypedScalar (ScalarUInt width))
+          | recordGrammar == grammar -> Just $ Text.intercalate ":"
+              [ "abi-v1"
+              , "field"
+              , grammar
+              , fieldName
+              , unValueId recordValue
+              , unValueId outputValue
+              , Text.pack (show width)
+              ]
+        _ -> Nothing
+
+splitSemanticField :: Text -> Maybe (Text, Text)
+splitSemanticField value = case Text.splitOn "." value of
+  [grammar, fieldName]
+    | not (Text.null grammar) && not (Text.null fieldName) -> Just (grammar, fieldName)
+  _ -> Nothing
+
+recognitionRuntimeName :: SystemsFunction -> ValueId -> RuntimeSiteRef -> Text
+recognitionRuntimeName functionValue pending site = case runtimeSiteKind site of
+  RecognitionBoundary grammar -> Text.intercalate ":"
+    [ "abi-v1"
+    , "recognize"
+    , grammar
+    , unValueId (recordValueForGrammar functionValue grammar pending)
+    ]
+  _ -> siteName site
+
+recordValueForGrammar :: SystemsFunction -> Text -> ValueId -> ValueId
+recordValueForGrammar functionValue grammar pending =
+  case
+    [ valueId
+    | (valueId, SystemsValue { systemsValueRole = RuntimeRecord candidate }) <-
+        Map.toAscList (systemsFunctionValues functionValue)
+    , candidate == grammar
+    ] of
+      [valueId] -> valueId
+      _ -> ValueId ("phil.recognized." <> grammar <> "." <> unValueId pending)
+
+isU64Value :: SystemsFunction -> ValueId -> Bool
+isU64Value functionValue valueId =
+  case Map.lookup valueId (systemsFunctionValues functionValue) of
+    Just SystemsValue { systemsValueRole = TypedScalar (ScalarUInt 64) } -> True
+    _ -> False
+
+isRecognizedRecordV1 :: LLVMTargetProfile -> Bool
+isRecognizedRecordV1 target =
+  llvmTargetRuntimeABIProfile target == "phil-runtime/phase0/recognized-record-v1"
 
 runtimeBranch :: RuntimeSiteRef -> BlockId -> BlockId -> LLVMTerminator
 runtimeBranch site yes no =
