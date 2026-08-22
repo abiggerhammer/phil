01c47d6a40b051447f6edaf9d3e1ba8c254d794f
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 12:23:22 +0200

    Export exact-receive LLVM candidate

--- src/Phil/LLVM.hs
+++ src/Phil/LLVM.hs
@@ -6,8 +6,10 @@ module Phil.LLVM
   , module Phil.LLVM.RecognizedRecord
   , module Phil.LLVM.RecognizedRecordCertification
   , module Phil.LLVM.RecognizedRecordProofCertification
+  , module Phil.LLVM.ExactReceive
   ) where
 
+import Phil.LLVM.ExactReceive
 import Phil.LLVM.IR
 import Phil.LLVM.Lower
 import Phil.LLVM.Phase0
