435debbc92aa9584e27c24c092a5d71094b15779
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 20:42:07 +0200

    Import scalar dataflow identity for LLVM storage proof

--- proof/Phil/LLVM/Storage.v
+++ proof/Phil/LLVM/Storage.v
@@ -1,4 +1,4 @@
-From Phil.Systems Require Import Identity Storage.
+From Phil.Systems Require Import ScalarDataflow Storage.
 From Phil.LLVM Require Import DigestValidation RuntimeSymbolIdentity.
 
 (*
