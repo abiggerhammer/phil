b1b0e5776ea3f05104ed556cd9957c3e56559fb8
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 06:11:16 +0200

    Expose assurance ledger API

---
+++ src/Phil/Assurance.hs
@@ -0,0 +1,9 @@
+module Phil.Assurance
+  ( module Phil.Assurance.Types
+  , module Phil.Assurance.Verify
+  , module Phil.Assurance.Phase0
+  ) where
+
+import Phil.Assurance.Phase0
+import Phil.Assurance.Types
+import Phil.Assurance.Verify
