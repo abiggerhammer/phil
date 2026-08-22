0e9b4dc3b904f8ecbd03c4670a715b9596bcd36d
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 06:25:07 +0200

    Expose Core-to-ledger handoff

--- src/Phil/Assurance.hs
+++ src/Phil/Assurance.hs
@@ -1,9 +1,11 @@
 module Phil.Assurance
   ( module Phil.Assurance.Types
+  , module Phil.Assurance.Handoff
   , module Phil.Assurance.Verify
   , module Phil.Assurance.Phase0
   ) where
 
+import Phil.Assurance.Handoff
 import Phil.Assurance.Phase0
 import Phil.Assurance.Types
 import Phil.Assurance.Verify
