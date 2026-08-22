ca6b63aab80670651259b2239ba204b8f53f0996
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 19:20:45 +0200

    Export storage Systems verifier

--- src/Phil/Systems.hs
+++ src/Phil/Systems.hs
@@ -6,6 +6,7 @@ module Phil.Systems
   , module Phil.Systems.FieldProjection
   , module Phil.Systems.RecognizedRecord
   , module Phil.Systems.DigestValidation
+  , module Phil.Systems.Storage
   ) where
 
 import Phil.Systems.Dataflow
@@ -14,4 +15,5 @@ import Phil.Systems.FieldProjection
 import Phil.Systems.IR
 import Phil.Systems.Phase0
 import Phil.Systems.RecognizedRecord
+import Phil.Systems.Storage
 import Phil.Systems.Verify
