a838b64932759a2c8ed2513872541b2ef04847e8
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-22 11:15:34 +0200

    Export recognized-record LLVM certification

--- src/Phil/LLVM.hs
+++ src/Phil/LLVM.hs
@@ -4,10 +4,12 @@ module Phil.LLVM
   , module Phil.LLVM.Verify
   , module Phil.LLVM.Phase0
   , module Phil.LLVM.RecognizedRecord
+  , module Phil.LLVM.RecognizedRecordCertification
   ) where
 
 import Phil.LLVM.IR
 import Phil.LLVM.Lower
 import Phil.LLVM.Phase0
 import Phil.LLVM.RecognizedRecord
+import Phil.LLVM.RecognizedRecordCertification
 import Phil.LLVM.Verify
