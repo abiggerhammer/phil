f6a7ebdc9de349804c8bcf4863de6ce0e4829f22
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-27 21:06:49 +0200

    Add DATA-009 generic mode propagation conformance (#284)
    * Add symbolic generic mode propagation
    
    * Add DATA-009 generic mode conformance corpus
    
    * Run DATA-009 in shared data conformance

--- src/Phil/Core/DataMode.hs
+++ src/Phil/Core/DataMode.hs
@@ -1,10 +1,18 @@
 module Phil.Core.DataMode
-  ( modeLub
+  ( ModeExpr (..)
+  , modeLub
   , deriveRecordMode
   , deriveSumMode
+  , instantiateMode
   ) where
 
 import Phil.Core.Syntax (Mode (..))
+
+data ModeExpr
+  = FixedMode Mode
+  | ParameterMode String
+  | StrongestMode [ModeExpr]
+  deriving (Eq, Show)
 
 modeLub :: Mode -> Mode -> Mode
 modeLub left right = case (left, right) of
@@ -19,3 +27,13 @@ deriveRecordMode = foldr modeLub Unrestricted
 
 deriveSumMode :: [[Mode]] -> Mode
 deriveSumMode = foldr (modeLub . deriveRecordMode) Unrestricted
+
+instantiateMode :: [(String, Mode)] -> ModeExpr -> Either String Mode
+instantiateMode environment expression = case expression of
+  FixedMode mode -> Right mode
+  ParameterMode parameter ->
+    case lookup parameter environment of
+      Just mode -> Right mode
+      Nothing -> Left ("unknown generic mode parameter: " <> parameter)
+  StrongestMode expressions ->
+    foldr modeLub Unrestricted <$> traverse (instantiateMode environment) expressions
