8cd76d74c336ed8739ee04e26ecada94766bf5a2
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-27 22:40:56 +0200

    Add BND-004 exact boundary mapping gate

---
+++ src/Phil/Core/BoundaryMapping.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Phil.Core.BoundaryMapping
+  ( BoundaryRepresentationId (..)
+  , ValueTypeRevision (..)
+  , BoundaryRepresentation (..)
+  , BoundaryMappingRequest (..)
+  , CorrespondenceEvidence (..)
+  , BoundaryMappingError (..)
+  , mapRecognizedBoundary
+  ) where
+
+import Data.Text (Text)
+import Phil.Core.Recognition
+  ( ParsedWitness
+  , parsedGrammarId
+  , parsedValueName
+  )
+import Phil.Core.Syntax (GrammarId, Name)
+
+newtype BoundaryRepresentationId = BoundaryRepresentationId Text
+  deriving (Eq, Ord, Show)
+
+newtype ValueTypeRevision = ValueTypeRevision Text
+  deriving (Eq, Ord, Show)
+
+data BoundaryRepresentation = BoundaryRepresentation
+  { representationId :: BoundaryRepresentationId
+  , representationGrammar :: GrammarId
+  , representationValueType :: ValueTypeRevision
+  }
+  deriving (Eq, Show)
+
+data BoundaryMappingRequest = BoundaryMappingRequest
+  { requestedRepresentation :: BoundaryRepresentationId
+  , requestedGrammar :: GrammarId
+  , requestedValueType :: ValueTypeRevision
+  , requestedGrammarValue :: Name
+  , requestedSemanticValue :: Name
+  }
+  deriving (Eq, Show)
+
+data CorrespondenceEvidence = CorrespondenceEvidence
+  { correspondenceRepresentation :: BoundaryRepresentationId
+  , correspondenceGrammar :: GrammarId
+  , correspondenceValueType :: ValueTypeRevision
+  , correspondenceGrammarValue :: Name
+  , correspondenceSemanticValue :: Name
+  }
+  deriving (Eq, Show)
+
+data BoundaryMappingError
+  = BoundaryRepresentationMismatch BoundaryRepresentationId BoundaryRepresentationId
+  | BoundaryGrammarMismatch GrammarId GrammarId
+  | BoundaryValueTypeMismatch ValueTypeRevision ValueTypeRevision
+  | RecognizedGrammarMismatch GrammarId GrammarId
+  | RecognizedValueMismatch Name Name
+  deriving (Eq, Show)
+
+mapRecognizedBoundary
+  :: BoundaryRepresentation
+  -> ParsedWitness
+  -> BoundaryMappingRequest
+  -> Either BoundaryMappingError CorrespondenceEvidence
+mapRecognizedBoundary representation parsed request
+  | requestedRepresentation request /= representationId representation =
+      Left (BoundaryRepresentationMismatch (representationId representation) (requestedRepresentation request))
+  | requestedGrammar request /= representationGrammar representation =
+      Left (BoundaryGrammarMismatch (representationGrammar representation) (requestedGrammar request))
+  | requestedValueType request /= representationValueType representation =
+      Left (BoundaryValueTypeMismatch (representationValueType representation) (requestedValueType request))
+  | parsedGrammarId parsed /= representationGrammar representation =
+      Left (RecognizedGrammarMismatch (representationGrammar representation) (parsedGrammarId parsed))
+  | parsedValueName parsed /= requestedGrammarValue request =
+      Left (RecognizedValueMismatch (parsedValueName parsed) (requestedGrammarValue request))
+  | otherwise = Right CorrespondenceEvidence
+      { correspondenceRepresentation = representationId representation
+      , correspondenceGrammar = representationGrammar representation
+      , correspondenceValueType = representationValueType representation
+      , correspondenceGrammarValue = requestedGrammarValue request
+      , correspondenceSemanticValue = requestedSemanticValue request
+      }
