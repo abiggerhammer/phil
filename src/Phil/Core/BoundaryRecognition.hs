68a73cdd9a5ec2bffcbf497263e608d9d46ed378
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-27 22:25:17 +0200

    Add BND-003 malformed complete-frame rejection path

--- src/Phil/Core/BoundaryRecognition.hs
+++ src/Phil/Core/BoundaryRecognition.hs
@@ -2,13 +2,17 @@ module Phil.Core.BoundaryRecognition
   ( RecognitionExtent (..)
   , CompleteRecognitionError (..)
   , recognizeCompleteFrame
+  , rejectMalformedCompleteFrame
   ) where
 
+import Data.Text (Text)
 import Phil.Core.Context (ResourceContext)
 import Phil.Core.Recognition
   ( ParsedWitness
   , PendingRawView
   , RecognitionError
+  , RecognitionFailure
+  , trustedRecognitionFailure
   , trustedRecognitionSuccess
   )
 import Phil.Core.Syntax (Name)
@@ -32,16 +36,33 @@ recognizeCompleteFrame
   -> RecognitionExtent
   -> ResourceContext
   -> Either CompleteRecognitionError ParsedWitness
-recognizeCompleteFrame raw valueName extent context
+recognizeCompleteFrame raw valueName extent context = do
+  checkCompleteExtent extent
+  mapLeft UnderlyingRecognitionError
+    (trustedRecognitionSuccess raw valueName context)
+
+rejectMalformedCompleteFrame
+  :: PendingRawView
+  -> Text
+  -> RecognitionExtent
+  -> ResourceContext
+  -> Either CompleteRecognitionError RecognitionFailure
+rejectMalformedCompleteFrame raw detail extent context = do
+  checkCompleteExtent extent
+  mapLeft UnderlyingRecognitionError
+    (trustedRecognitionFailure raw detail context)
+
+checkCompleteExtent
+  :: RecognitionExtent
+  -> Either CompleteRecognitionError ()
+checkCompleteExtent extent
   | declaredFrameBytes extent < 0 || consumedFrameBytes extent < 0 =
       Left (InvalidRecognitionExtent extent)
   | consumedFrameBytes extent < declaredFrameBytes extent =
       Left (TrailingBytesInsideFrame extent)
   | consumedFrameBytes extent > declaredFrameBytes extent =
       Left (RecognitionConsumedPastFrame extent)
-  | otherwise =
-      mapLeft UnderlyingRecognitionError
-        (trustedRecognitionSuccess raw valueName context)
+  | otherwise = Right ()
 
 mapLeft :: (a -> b) -> Either a c -> Either b c
 mapLeft f = either (Left . f) Right
