787be10c694d36c2774197a055077fe8f7e2b957
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-27 17:23:34 +0200

    Simplify structural requirement equality proof

--- proof/Phil/Core/GenericInstantiationDomainImplementation.v
+++ proof/Phil/Core/GenericInstantiationDomainImplementation.v
@@ -224,15 +224,8 @@ Proof.
   intros left right.
   destruct left as [leftPermission | leftInterface | leftProposition];
     destruct right as [rightPermission | rightInterface | rightProposition]; cbn.
-  - split.
-    + intro H.
-      apply structural_permission_eqb_true_iff in H.
-      subst rightPermission.
-      reflexivity.
-    + intro H.
-      inversion H.
-      apply (proj2 (structural_permission_eqb_true_iff leftPermission leftPermission)).
-      reflexivity.
+  - destruct leftPermission, rightPermission; cbn; split; intro H;
+      try reflexivity; try discriminate.
   - split; intro H; discriminate.
   - split; intro H; discriminate.
   - split; intro H; discriminate.
@@ -258,7 +251,8 @@ Proof.
 Qed.
 
 Lemma in_disposition_keys_iff :
-  forall (requirement : GenericRequirement)
+  forall
+    (requirement : GenericRequirement)
     (dispositions : list (GenericRequirement * GenericRequirementDisposition)),
     In requirement (map fst dispositions) <->
     exists disposition, In (requirement, disposition) dispositions.
