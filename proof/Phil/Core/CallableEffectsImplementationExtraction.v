8143e281ab84e3b7dc82ea8713480e37caa63cdf
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-28 02:13:03 +0200

    Map extracted callable-effect booleans to Prelude

--- proof/Phil/Core/CallableEffectsImplementationExtraction.v
+++ proof/Phil/Core/CallableEffectsImplementationExtraction.v
@@ -3,6 +3,7 @@ From Stdlib Require Import Extraction.
 From Phil.Core Require Import CallableEffectsImplementation.
 
 Extraction Language Haskell.
+Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
 
 Extraction "CallableEffectKernel.hs"
   CallableUseEffectKind
