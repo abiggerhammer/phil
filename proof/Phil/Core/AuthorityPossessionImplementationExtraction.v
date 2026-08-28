d2bff82299c2a195a0d9db1eaf3831143e7139d8
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-28 23:22:49 +0200

    Map authority extraction bool to Prelude.Bool

--- proof/Phil/Core/AuthorityPossessionImplementationExtraction.v
+++ proof/Phil/Core/AuthorityPossessionImplementationExtraction.v
@@ -3,6 +3,10 @@ From Phil.Core Require Import AuthorityPossessionImplementation.
 
 Extraction Language Haskell.
 
+(* Keep the executable representation aligned with native Haskell facts, as in
+   GenericStructuralImplementationExtraction.v. *)
+Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
+
 Extraction "AuthorityPossessionKernel"
   AuthorityExerciseDecision
   decideAuthorityExerciseFacts
