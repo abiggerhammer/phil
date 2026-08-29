915e8c2b35ad8414ae23f3ba48f3cc1269720394
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-29 05:42:29 +0200

    Check in assurance validity-scope kernel

---
+++ src/AssuranceValidityScopeKernel.hs
@@ -0,0 +1,25 @@
+module AssuranceValidityScopeKernel where
+
+import qualified Prelude
+
+andb :: Prelude.Bool -> Prelude.Bool -> Prelude.Bool
+andb b1 b2 =
+  case b1 of {
+   Prelude.True -> b2;
+   Prelude.False -> Prelude.False}
+
+validityScopeFactsb :: ([] Prelude.Bool) -> Prelude.Bool
+validityScopeFactsb facts =
+  case facts of {
+   [] -> Prelude.True;
+   (:) fact rest -> andb fact (validityScopeFactsb rest)}
+
+data ValidityScopeDecision =
+   ValidityScopeAccepted
+ | ValidityScopeRejected
+
+decideValidityScope :: ([] Prelude.Bool) -> ValidityScopeDecision
+decideValidityScope facts =
+  case validityScopeFactsb facts of {
+   Prelude.True -> ValidityScopeAccepted;
+   Prelude.False -> ValidityScopeRejected}
