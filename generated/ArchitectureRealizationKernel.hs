cf8a281d71ea190dfabb4cb4aec8deeaa6d747d2
Meredith L. Patterson <clonearmy@gmail.com>
2026-08-30 04:19:21 +0200

    Check in exact architecture realization kernel

---
+++ generated/ArchitectureRealizationKernel.hs
@@ -0,0 +1,12 @@
+module ArchitectureRealizationKernel where
+
+import qualified Prelude
+
+data ArchitectureRealizationPlan key revision semantics =
+   MkArchitectureRealizationPlan key revision semantics
+
+planArchitectureRealization :: a1 -> a2 -> a3 -> ArchitectureRealizationPlan
+                               a1 a2 a3
+planArchitectureRealization instanceKey instanceRevision semantics =
+  MkArchitectureRealizationPlan instanceKey instanceRevision semantics
+
