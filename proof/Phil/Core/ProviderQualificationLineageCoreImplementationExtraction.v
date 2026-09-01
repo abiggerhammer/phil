74885e492606b0d6dd45319f98596a10045927b0
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-01 06:38:41 +0200

    Map provider lineage extraction to native Haskell types

--- proof/Phil/Core/ProviderQualificationLineageCoreImplementationExtraction.v
+++ proof/Phil/Core/ProviderQualificationLineageCoreImplementationExtraction.v
@@ -3,6 +3,9 @@ From Phil.Core Require Import ProviderQualificationLineageCoreImplementation.
 
 Extraction Language Haskell.
 
+Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
+Extract Inductive list => "[]" [ "[]" "(:)" ].
+
 Extraction "ProviderQualificationLineageCoreKernel.hs"
   decideQualificationIdentityByFacts
   decideQualificationRegistryByFacts
