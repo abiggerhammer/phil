6eeb10b8621c26cdc392b2fb4d84395e1839559d
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-02 15:03:59 +0200

    Fix Runtime Graph extraction setup

--- proof/Phil/Core/SystemsRuntimeGraphImplementationExtraction.v
+++ proof/Phil/Core/SystemsRuntimeGraphImplementationExtraction.v
@@ -1,9 +1,11 @@
+From Stdlib Require Import Extraction.
+
 From Phil.Core Require Import SystemsRuntimeGraphImplementation.
 
 Extraction Language Haskell.
-Extract Inductive bool => "Prelude.Bool" ["Prelude.True" "Prelude.False"].
+Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
 
-Extraction "SystemsRuntimeGraphKernel.hs"
+Extraction "SystemsRuntimeGraphKernel"
   decideRuntimeClaimGraphByFacts
   decideRuntimePrimitiveReuseByFacts
   decideRuntimeCostAttributionByFacts
