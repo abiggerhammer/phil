b5ab7aab5e956f39dad7b0c95de04c3bea3cb61f
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-02 12:30:13 +0200

    Make deployment qualification proof structurally robust

--- proof/Phil/Assurance/DeploymentQualification.v
+++ proof/Phil/Assurance/DeploymentQualification.v
@@ -82,59 +82,7 @@ Definition DeploymentDomainEvidenceRegistry :=
 Definition DeploymentCompositionEvidenceRegistry :=
   DeploymentCompositionEvidenceId -> option DeploymentCompositionEvidenceModel.
 
-Definition DeploymentPlanWellFormed
-  (plan : DeploymentPlanModel) : Prop :=
-  planTopologyIdentityValid plan = true /\
-  planLinksWellFormed plan = true /\
-  (forall claim,
-    planClaim plan claim = true ->
-    exists domain,
-      planDomain plan domain = true /\
-      planClaimDomain plan claim domain = true) /\
-  (forall claim domain,
-    planClaimDomain plan claim domain = true ->
-    planClaim plan claim = true /\
-    planDomain plan domain = true).
-
-Definition DeploymentQualificationMatchesPlan
-  (plan : DeploymentPlanModel)
-  (qualification : DeploymentQualificationModel) : Prop :=
-  qualificationArtifact qualification = planArtifact plan /\
-  qualificationPolicy qualification = planPolicy plan /\
-  qualificationTopologyRevision qualification = planTopologyRevision plan /\
-  (forall claim,
-    qualificationClaim qualification claim = planClaim plan claim) /\
-  qualificationIdentityValid qualification = true.
-
-Definition DeploymentDomainEvidenceValid
-  (current : DeploymentObservation)
-  (plan : DeploymentPlanModel)
-  (registry : DeploymentDomainEvidenceRegistry)
-  (qualification : DeploymentQualificationModel) : Prop :=
-  (forall domain,
-    planDomain plan domain = true ->
-    exists key evidence,
-      qualificationDomainEvidence qualification domain = Some key /\
-      registry key = Some evidence /\
-      domainEvidenceDomain evidence = domain /\
-      domainEvidenceArtifact evidence = planArtifact plan /\
-      domainEvidencePolicy evidence = planPolicy plan /\
-      PointWithin current
-        (domainEvidenceValidFrom evidence)
-        (domainEvidenceValidUntil evidence) /\
-      IntervalWithin
-        (qualificationValidFrom qualification)
-        (qualificationValidUntil qualification)
-        (domainEvidenceValidFrom evidence)
-        (domainEvidenceValidUntil evidence) /\
-      (forall claim,
-        planClaimDomain plan claim domain = true ->
-        domainEvidenceClaim evidence claim = true)) /\
-  (forall domain key,
-    qualificationDomainEvidence qualification domain = Some key ->
-    planDomain plan domain = true).
-
-Definition DeploymentCompositionEvidenceValid
+Definition CompositeEvidenceRequirement
   (current : DeploymentObservation)
   (plan : DeploymentPlanModel)
   (registry : DeploymentCompositionEvidenceRegistry)
@@ -164,19 +112,79 @@ Definition DeploymentCompositionEvidenceValid
   | false => qualificationCompositionEvidence qualification = None
   end.
 
-Definition DeploymentQualificationValid
+Record DeploymentQualificationValid
   (current : DeploymentObservation)
   (plan : DeploymentPlanModel)
   (domainRegistry : DeploymentDomainEvidenceRegistry)
   (compositionRegistry : DeploymentCompositionEvidenceRegistry)
   (qualification : DeploymentQualificationModel) : Prop :=
-  DeploymentPlanWellFormed plan /\
-  DeploymentQualificationMatchesPlan plan qualification /\
-  PointWithin current
-    (qualificationValidFrom qualification)
-    (qualificationValidUntil qualification) /\
-  DeploymentDomainEvidenceValid current plan domainRegistry qualification /\
-  DeploymentCompositionEvidenceValid current plan compositionRegistry qualification.
+  mkDeploymentQualificationValid {
+    qualifiedTopologyIdentityValid : planTopologyIdentityValid plan = true;
+    qualifiedLinksWellFormed : planLinksWellFormed plan = true;
+
+    qualifiedClaimDomainsTotal :
+      forall claim,
+        planClaim plan claim = true ->
+        exists domain,
+          planDomain plan domain = true /\
+          planClaimDomain plan claim domain = true;
+
+    qualifiedClaimDomainsSound :
+      forall claim domain,
+        planClaimDomain plan claim domain = true ->
+        planClaim plan claim = true /\
+        planDomain plan domain = true;
+
+    qualifiedExactArtifact :
+      qualificationArtifact qualification = planArtifact plan;
+
+    qualifiedExactPolicy :
+      qualificationPolicy qualification = planPolicy plan;
+
+    qualifiedExactTopology :
+      qualificationTopologyRevision qualification = planTopologyRevision plan;
+
+    qualifiedExactClaimSet :
+      forall claim,
+        qualificationClaim qualification claim = planClaim plan claim;
+
+    qualifiedIdentityValid : qualificationIdentityValid qualification = true;
+
+    qualifiedCurrent :
+      PointWithin current
+        (qualificationValidFrom qualification)
+        (qualificationValidUntil qualification);
+
+    qualifiedEverySelectedDomainHasEvidence :
+      forall domain,
+        planDomain plan domain = true ->
+        exists key evidence,
+          qualificationDomainEvidence qualification domain = Some key /\
+          domainRegistry key = Some evidence /\
+          domainEvidenceDomain evidence = domain /\
+          domainEvidenceArtifact evidence = planArtifact plan /\
+          domainEvidencePolicy evidence = planPolicy plan /\
+          PointWithin current
+            (domainEvidenceValidFrom evidence)
+            (domainEvidenceValidUntil evidence) /\
+          IntervalWithin
+            (qualificationValidFrom qualification)
+            (qualificationValidUntil qualification)
+            (domainEvidenceValidFrom evidence)
+            (domainEvidenceValidUntil evidence) /\
+          (forall claim,
+            planClaimDomain plan claim domain = true ->
+            domainEvidenceClaim evidence claim = true);
+
+    qualifiedNoExtraDomainBinding :
+      forall domain key,
+        qualificationDomainEvidence qualification domain = Some key ->
+        planDomain plan domain = true;
+
+    qualifiedCompositionEvidence :
+      CompositeEvidenceRequirement
+        current plan compositionRegistry qualification
+  }.
 
 Definition DeploymentQualificationAvailable
   (current : DeploymentObservation)
@@ -201,8 +209,12 @@ Theorem qualified_deployment_uses_exact_plan_identity :
     qualificationIdentityValid qualification = true.
 Proof.
   intros current plan domainRegistry compositionRegistry qualification Hvalid.
-  destruct Hvalid as [_ [Hmatches _]].
-  exact Hmatches.
+  repeat split.
+  - exact (qualifiedExactArtifact current plan domainRegistry compositionRegistry qualification Hvalid).
+  - exact (qualifiedExactPolicy current plan domainRegistry compositionRegistry qualification Hvalid).
+  - exact (qualifiedExactTopology current plan domainRegistry compositionRegistry qualification Hvalid).
+  - exact (qualifiedExactClaimSet current plan domainRegistry compositionRegistry qualification Hvalid).
+  - exact (qualifiedIdentityValid current plan domainRegistry compositionRegistry qualification Hvalid).
 Qed.
 
 Theorem raw_attestation_without_qualification_cannot_close :
@@ -211,7 +223,8 @@ Theorem raw_attestation_without_qualification_cannot_close :
         current plan domainRegistry compositionRegistry None.
 Proof.
   intros current plan domainRegistry compositionRegistry Havailable.
-  destruct Havailable as [qualification [Hpresent _]].
+  destruct Havailable as [qualification Havailable].
+  destruct Havailable as [Hpresent Hvalid].
   discriminate Hpresent.
 Qed.
 
@@ -222,12 +235,9 @@ Theorem qualification_artifact_mismatch_cannot_qualify :
         current plan domainRegistry compositionRegistry qualification.
 Proof.
   intros current plan domainRegistry compositionRegistry qualification Hmismatch Hvalid.
-  pose proof
-    (qualified_deployment_uses_exact_plan_identity
-      current plan domainRegistry compositionRegistry qualification Hvalid)
-    as Hidentity.
-  destruct Hidentity as [Hartifact _].
-  exact (Hmismatch Hartifact).
+  apply Hmismatch.
+  exact (qualifiedExactArtifact
+    current plan domainRegistry compositionRegistry qualification Hvalid).
 Qed.
 
 Theorem qualified_deployment_is_current_at_explicit_observation :
@@ -239,8 +249,8 @@ Theorem qualified_deployment_is_current_at_explicit_observation :
       (qualificationValidUntil qualification).
 Proof.
   intros current plan domainRegistry compositionRegistry qualification Hvalid.
-  destruct Hvalid as [_ [_ [Hcurrent _]]].
-  exact Hcurrent.
+  exact (qualifiedCurrent
+    current plan domainRegistry compositionRegistry qualification Hvalid).
 Qed.
 
 Theorem stale_qualification_cannot_qualify :
@@ -253,7 +263,7 @@ Theorem stale_qualification_cannot_qualify :
 Proof.
   intros current plan domainRegistry compositionRegistry qualification Hstale Hvalid.
   apply Hstale.
-  exact (qualified_deployment_is_current_at_explicit_observation
+  exact (qualifiedCurrent
     current plan domainRegistry compositionRegistry qualification Hvalid).
 Qed.
 
@@ -281,9 +291,8 @@ Theorem every_selected_domain_has_exact_evidence :
         domainEvidenceClaim evidence claim = true).
 Proof.
   intros current plan domainRegistry compositionRegistry qualification domain Hvalid Hdomain.
-  destruct Hvalid as [_ [_ [_ [Hdomains _]]]].
-  destruct Hdomains as [Htotal _].
-  exact (Htotal domain Hdomain).
+  exact (qualifiedEverySelectedDomainHasEvidence
+    current plan domainRegistry compositionRegistry qualification Hvalid domain Hdomain).
 Qed.
 
 Theorem no_unselected_domain_can_gain_evidence_binding :
@@ -294,72 +303,8 @@ Theorem no_unselected_domain_can_gain_evidence_binding :
     planDomain plan domain = true.
 Proof.
   intros current plan domainRegistry compositionRegistry qualification domain key Hvalid Hbinding.
-  destruct Hvalid as [_ [_ [_ [Hdomains _]]]].
-  destruct Hdomains as [_ Hnoextra].
-  exact (Hnoextra domain key Hbinding).
-Qed.
-
-Theorem selected_domain_evidence_is_current :
-  forall current plan domainRegistry compositionRegistry qualification domain,
-    DeploymentQualificationValid
-      current plan domainRegistry compositionRegistry qualification ->
-    planDomain plan domain = true ->
-    exists key evidence,
-      qualificationDomainEvidence qualification domain = Some key /\
-      domainRegistry key = Some evidence /\
-      PointWithin current
-        (domainEvidenceValidFrom evidence)
-        (domainEvidenceValidUntil evidence).
-Proof.
-  intros current plan domainRegistry compositionRegistry qualification domain Hvalid Hdomain.
-  destruct (every_selected_domain_has_exact_evidence
-    current plan domainRegistry compositionRegistry qualification domain Hvalid Hdomain)
-    as [key [evidence [Hbinding [Hregistry [_ [_ [_ [Hcurrent _]]]]]]]]].
-  exists key, evidence.
-  repeat split; assumption.
-Qed.
-
-Theorem qualification_never_outlives_selected_domain_evidence :
-  forall current plan domainRegistry compositionRegistry qualification domain,
-    DeploymentQualificationValid
-      current plan domainRegistry compositionRegistry qualification ->
-    planDomain plan domain = true ->
-    exists key evidence,
-      qualificationDomainEvidence qualification domain = Some key /\
-      domainRegistry key = Some evidence /\
-      IntervalWithin
-        (qualificationValidFrom qualification)
-        (qualificationValidUntil qualification)
-        (domainEvidenceValidFrom evidence)
-        (domainEvidenceValidUntil evidence).
-Proof.
-  intros current plan domainRegistry compositionRegistry qualification domain Hvalid Hdomain.
-  destruct (every_selected_domain_has_exact_evidence
-    current plan domainRegistry compositionRegistry qualification domain Hvalid Hdomain)
-    as [key [evidence [Hbinding [Hregistry [_ [_ [_ [_ [Hinterval _]]]]]]]]].
-  exists key, evidence.
-  repeat split; assumption.
-Qed.
-
-Theorem domain_evidence_covers_every_assigned_claim :
-  forall current plan domainRegistry compositionRegistry qualification domain claim,
-    DeploymentQualificationValid
-      current plan domainRegistry compositionRegistry qualification ->
-    planDomain plan domain = true ->
-    planClaimDomain plan claim domain = true ->
-    exists key evidence,
-      qualificationDomainEvidence qualification domain = Some key /\
-      domainRegistry key = Some evidence /\
-      domainEvidenceClaim evidence claim = true.
-Proof.
-  intros current plan domainRegistry compositionRegistry qualification domain claim
-    Hvalid Hdomain Hassigned.
-  destruct (every_selected_domain_has_exact_evidence
-    current plan domainRegistry compositionRegistry qualification domain Hvalid Hdomain)
-    as [key [evidence [Hbinding [Hregistry [_ [_ [_ [_ [_ Hclaims]]]]]]]]].
-  exists key, evidence.
-  repeat split; try assumption.
-  exact (Hclaims claim Hassigned).
+  exact (qualifiedNoExtraDomainBinding
+    current plan domainRegistry compositionRegistry qualification Hvalid domain key Hbinding).
 Qed.
 
 Theorem composite_deployment_has_explicit_composition_evidence :
@@ -389,8 +334,11 @@ Theorem composite_deployment_has_explicit_composition_evidence :
         compositionEvidenceClaim evidence claim = planClaim plan claim).
 Proof.
   intros current plan domainRegistry compositionRegistry qualification Hvalid Hcomposite.
-  destruct Hvalid as [_ [_ [_ [_ Hcomposition]]]].
-  unfold DeploymentCompositionEvidenceValid in Hcomposition.
+  pose proof
+    (qualifiedCompositionEvidence
+      current plan domainRegistry compositionRegistry qualification Hvalid)
+    as Hcomposition.
+  unfold CompositeEvidenceRequirement in Hcomposition.
   rewrite Hcomposite in Hcomposition.
   exact Hcomposition.
 Qed.
@@ -403,35 +351,15 @@ Theorem independent_domain_attestations_cannot_replace_composition_evidence :
         current plan domainRegistry compositionRegistry qualification.
 Proof.
   intros current plan domainRegistry compositionRegistry qualification Hcomposite Hmissing Hvalid.
-  destruct (composite_deployment_has_explicit_composition_evidence
-    current plan domainRegistry compositionRegistry qualification Hvalid Hcomposite)
-    as [key [evidence [Hbinding _]]].
+  pose proof
+    (composite_deployment_has_explicit_composition_evidence
+      current plan domainRegistry compositionRegistry qualification Hvalid Hcomposite)
+    as Hcomposition.
+  destruct Hcomposition as [key Hcomposition].
+  destruct Hcomposition as [evidence Hcomposition].
+  destruct Hcomposition as [Hbinding Hrest].
   rewrite Hmissing in Hbinding.
-  discriminate.
-Qed.
-
-Theorem composition_evidence_matches_complete_topology :
-  forall current plan domainRegistry compositionRegistry qualification,
-    DeploymentQualificationValid
-      current plan domainRegistry compositionRegistry qualification ->
-    planComposite plan = true ->
-    exists key evidence,
-      qualificationCompositionEvidence qualification = Some key /\
-      compositionRegistry key = Some evidence /\
-      (forall domain,
-        compositionEvidenceDomain evidence domain = planDomain plan domain) /\
-      (forall link,
-        compositionEvidenceLink evidence link = planLink plan link) /\
-      (forall claim,
-        compositionEvidenceClaim evidence claim = planClaim plan claim).
-Proof.
-  intros current plan domainRegistry compositionRegistry qualification Hvalid Hcomposite.
-  destruct (composite_deployment_has_explicit_composition_evidence
-    current plan domainRegistry compositionRegistry qualification Hvalid Hcomposite)
-    as [key [evidence
-      [Hbinding [Hregistry [_ [_ [_ [_ [_ [Hdomains [Hlinks Hclaims]]]]]]]]]]].
-  exists key, evidence.
-  repeat split; assumption.
+  discriminate Hbinding.
 Qed.
 
 Theorem noncomposite_deployment_rejects_unexpected_composition_evidence :
@@ -442,9 +370,12 @@ Theorem noncomposite_deployment_rejects_unexpected_composition_evidence :
         current plan domainRegistry compositionRegistry qualification.
 Proof.
   intros current plan domainRegistry compositionRegistry qualification key Hsimple Hunexpected Hvalid.
-  destruct Hvalid as [_ [_ [_ [_ Hcomposition]]]].
-  unfold DeploymentCompositionEvidenceValid in Hcomposition.
+  pose proof
+    (qualifiedCompositionEvidence
+      current plan domainRegistry compositionRegistry qualification Hvalid)
+    as Hcomposition.
+  unfold CompositeEvidenceRequirement in Hcomposition.
   rewrite Hsimple in Hcomposition.
   rewrite Hunexpected in Hcomposition.
-  discriminate.
+  discriminate Hcomposition.
 Qed.
