b8a05540eec1d4b9221148066a95c32b226ba612
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-03 20:30:47 +0200

    Use propositional closure for Callable Mode facts

--- proof/Phil/Core/CallableModeStrengtheningImplementation.v
+++ proof/Phil/Core/CallableModeStrengtheningImplementation.v
@@ -67,7 +67,7 @@ Proof.
     targetImplementationReason contractMatches detailPresent.
   destruct nonWeakening, equalToMinimum, justificationPresent,
     targetImplementationReason, contractMatches, detailPresent;
-    simpl; intuition congruence.
+    simpl; intuition.
 Qed.
 
 Theorem explicit_closure_mode_decision_accept_iff_certified :
@@ -134,7 +134,7 @@ Theorem checked_closure_mode_shape_accept_iff_facts :
 Proof.
   intros minimumExact selectedExact justificationExact.
   destruct minimumExact, selectedExact, justificationExact;
-    simpl; intuition congruence.
+    simpl; intuition.
 Qed.
 
 Theorem certified_explicit_closure_mode_supplies_checked_shape :
