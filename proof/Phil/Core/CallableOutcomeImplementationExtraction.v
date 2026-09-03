3cdfe16c756c084f8f7cd308bff95ca5a553cae6
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-03 22:00:31 +0200

    Extract Callable Outcome implementation kernel

---
+++ proof/Phil/Core/CallableOutcomeImplementationExtraction.v
@@ -0,0 +1,12 @@
+From Corelib Require Extraction.
+From Phil.Core Require Import CallableOutcomeImplementation.
+
+Extraction Language Haskell.
+
+Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
+
+Extraction "CallableOutcomeKernel"
+  OutcomeBucket
+  ResidualDisposition
+  CallableOutcomeDecision
+  decideCallableOutcomeByFacts.
