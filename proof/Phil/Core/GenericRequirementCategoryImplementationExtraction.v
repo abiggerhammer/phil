6bc11d2d11395206250d089ee5959b9939fe66bc
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-03 12:16:21 +0200

    Disambiguate Generic Requirement Category extraction objects

--- proof/Phil/Core/GenericRequirementCategoryImplementationExtraction.v
+++ proof/Phil/Core/GenericRequirementCategoryImplementationExtraction.v
@@ -8,10 +8,10 @@ Extraction Language Haskell.
 Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
 
 Extraction "GenericRequirementCategoryKernel"
-  GenericRequirementCategory
-  GenericRequirementCompetence
-  competenceForRequirementCategory
-  RequirementHandoffDecision
-  decideRequirementHandoffByFacts
-  RequirementInterfaceDomainDecision
-  decideRequirementInterfaceDomainByFacts.
+  Phil.Core.GenericRequirementCategory.GenericRequirementCategory
+  Phil.Core.GenericRequirementCategory.GenericRequirementCompetence
+  Phil.Core.GenericRequirementCategory.competenceForRequirementCategory
+  Phil.Core.GenericRequirementCategoryImplementation.RequirementHandoffDecision
+  Phil.Core.GenericRequirementCategoryImplementation.decideRequirementHandoffByFacts
+  Phil.Core.GenericRequirementCategoryImplementation.RequirementInterfaceDomainDecision
+  Phil.Core.GenericRequirementCategoryImplementation.decideRequirementInterfaceDomainByFacts.
