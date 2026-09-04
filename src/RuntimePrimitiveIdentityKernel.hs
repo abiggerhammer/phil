153df5f35e2a418540f0ce648d95e2ff0d5dfff7
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-05 00:41:59 +0200

    Add production target runtime primitive kernel copy

---
+++ src/RuntimePrimitiveIdentityKernel.hs
@@ -0,0 +1,14 @@
+module RuntimePrimitiveIdentityKernel where
+
+import qualified Prelude
+
+andb :: Prelude.Bool -> Prelude.Bool -> Prelude.Bool
+andb b1 b2 =
+  case b1 of {
+   Prelude.True -> b2;
+   Prelude.False -> Prelude.False}
+
+decideRuntimePrimitiveIdentityByFacts :: Prelude.Bool -> Prelude.Bool ->
+                                         Prelude.Bool
+decideRuntimePrimitiveIdentityByFacts =
+  andb
