31ac8a6c37f96940ae5b2d228f880c24e72d7778
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-04 05:53:23 +0200

    Import GrammarDerivation for structural scanner tokens

--- proof/Phil/Surface/GrammarDeterminacyStructuralScannerSoundness.v
+++ proof/Phil/Surface/GrammarDeterminacyStructuralScannerSoundness.v
@@ -1,6 +1,7 @@
 From Stdlib Require Import Arith.PeanoNat Bool.Bool Lists.List Strings.String.
 
 From Phil.Surface Require Import
+  GrammarDerivation
   GrammarDerivationOracle
   GrammarDeterminacyStructuralResolvers.
 
