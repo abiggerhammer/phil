6cdd16421d1900805dc620636fc8392b7ebb5c09
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-05 12:25:01 +0200

    Import FOLLOW facts directly

--- proof/Phil/Surface/GrammarDeterminacyAlternativeResolverContext.v
+++ proof/Phil/Surface/GrammarDeterminacyAlternativeResolverContext.v
@@ -6,6 +6,7 @@ From Phil.Surface Require Import
   GrammarDerivationOracle
   GrammarDeterminacyCertificate
   GrammarDeterminacyNullableFirst
+  GrammarDeterminacyFollowOverlap
   GrammarDeterminacySimpleResolvers
   GrammarDeterminacyStructuralResolvers
   GrammarDeterminacyPredictiveOracle
