b4d7d8cf468cd44a781d55f8aca74f40b0daec02
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-05 07:53:54 +0200

    Import trailing comma overlap list equality directly

--- proof/Phil/Surface/GrammarDeterminacyTrailingCommaConversion.v
+++ proof/Phil/Surface/GrammarDeterminacyTrailingCommaConversion.v
@@ -5,6 +5,7 @@ From Phil.Surface Require Import
   GrammarDerivation
   GrammarDerivationOracle
   GrammarDeterminacyNullableFirst
+  GrammarDeterminacyFollowOverlap
   GrammarDeterminacySimpleResolvers
   GrammarDeterminacySimpleResolverSoundness
   GrammarDeterminacyPredictiveOracle
