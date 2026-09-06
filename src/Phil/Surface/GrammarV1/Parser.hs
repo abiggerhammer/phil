c9b0681594e5a1d716fee91e4c4b355c2bea7027
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-06 09:53:49 +0000

    phase1: clean text literal parser pragmas

--- src/Phil/Surface/GrammarV1/Parser.hs
+++ src/Phil/Surface/GrammarV1/Parser.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Phil.Surface.GrammarV1.Parser
   ( GrammarV1ParseDiagnostic (..)
