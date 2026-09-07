f99c27b55150d4bdf61867356b1dd341c92c04e7
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-07 05:19:39 +0200

    Import trailing comma resolver definitions directly

--- proof/Phil/Surface/GrammarDeterminacyStructuralChoiceConversion.v
+++ proof/Phil/Surface/GrammarDeterminacyStructuralChoiceConversion.v
@@ -5,6 +5,7 @@ From Phil.Surface Require Import
   GrammarDerivation
   GrammarDerivationOracle
   GrammarDeterminacyNullableFirst
+  GrammarDeterminacySimpleResolvers
   GrammarDeterminacyPredictiveOracle
   GrammarDeterminacyContinuationSoundness
   GrammarDeterminacyDerivationPathSoundness
