0b720066642f573a6455c416a8a1540e179afecb
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-08 01:17:27 +0200

    test: remove unused IO-CODEC import

--- test/Phase1IOCodecUTF8Main.hs
+++ test/Phase1IOCodecUTF8Main.hs
@@ -31,7 +31,6 @@ import Phil.IO.FileSystem
   , FileReplaceOutcome (..)
   , FileSystemFailure (..)
   , FileSystemOperation (..)
-  , FileSystemState
   , checkFileSystemRead
   , checkFileSystemReplace
   , emptyFileSystemState
