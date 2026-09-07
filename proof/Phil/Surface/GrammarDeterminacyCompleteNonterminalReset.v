f555cd79958383047b8f20e28d117ba4e77f9c2b
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-07 02:23:13 +0200

    Import continuation lookahead definition directly

--- proof/Phil/Surface/GrammarDeterminacyCompleteNonterminalReset.v
+++ proof/Phil/Surface/GrammarDeterminacyCompleteNonterminalReset.v
@@ -5,6 +5,7 @@ From Phil.Surface Require Import
   GrammarDerivation
   GrammarDeterminacyNullableFirst
   GrammarDeterminacyFollowOverlap
+  GrammarDeterminacyContinuationSoundness
   GrammarDeterminacyFollowCoverage
   GrammarDeterminacyDerivationPathSoundness
   GrammarDeterminacyOracleAssemblyCoverage
