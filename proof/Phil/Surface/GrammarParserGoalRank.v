9def93522051aea124b04bcf209055c585631a1d
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-07 12:11:50 +0200

    Import GrammarDerivation for lookupRule

--- proof/Phil/Surface/GrammarParserGoalRank.v
+++ proof/Phil/Surface/GrammarParserGoalRank.v
@@ -2,6 +2,7 @@ From Stdlib Require Import Arith.PeanoNat Bool.Bool Lists.List Strings.String Li
 
 From Phil.Surface Require Import
   Grammar
+  GrammarDerivation
   GrammarDeterminacyNullableFirst
   GrammarParserRank.
 
