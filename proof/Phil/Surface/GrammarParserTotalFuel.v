9a71d4c458329a181db9ba56809b9431a0839fc4
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-08 04:49:40 +0200

    Import nullable fuel definitions explicitly

--- proof/Phil/Surface/GrammarParserTotalFuel.v
+++ proof/Phil/Surface/GrammarParserTotalFuel.v
@@ -5,6 +5,7 @@ From Phil.Surface Require Import
   GrammarDerivation
   GrammarDerivationOracle
   GrammarDeterminacyContinuationSoundness
+  GrammarDeterminacyNullableFirst
   GrammarDeterminacyMutualPredictiveBridge
   GrammarDeterminacyPredictiveOracle
   GrammarParserGoalRank
