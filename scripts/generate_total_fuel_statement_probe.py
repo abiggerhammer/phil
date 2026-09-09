bc5c418691ff0a69e270ad350cd9764d00341e0e
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-09 06:51:16 +0200

    Import expression fuel in statement probe

--- scripts/generate_total_fuel_statement_probe.py
+++ scripts/generate_total_fuel_statement_probe.py
@@ -7,6 +7,7 @@ From Phil.Surface Require Import
   Grammar
   GrammarDerivation
   GrammarDerivationOracle
+  GrammarDeterminacyNullableFirst
   GrammarDeterminacyPredictiveOracle
   GrammarParserGlobalGoalRank
   GrammarParserRecognizer
