d22d2d831ac037d3a88bfa0a08e88c2f7e140a06
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-10 05:20:57 +0200

    Keep fake recognizer compatible with tree bridge

--- test/fakes/SurfaceGrammarRecognizerKernel.hs
+++ test/fakes/SurfaceGrammarRecognizerKernel.hs
@@ -10,9 +10,32 @@ data String =
     EmptyString
   | String0 Ascii0 String
 
+data Nat =
+    O
+  | S Nat
+
 data ConcreteToken =
     TLiteral String
   | TLexical String String
+
+data ParseTree =
+    PTLiteral String
+  | PTLexical String String
+  | PTNonterminal String ParseTree
+  | PTSequence [ParseTree]
+  | PTAlternative Nat ParseTree
+  | PTOptionalNone
+  | PTOptionalSome ParseTree
+  | PTRepetition [ParseTree]
+
+data DerivationResult =
+    ResultTree ParseTree
+  | ResultTrees [ParseTree]
+
+phase1_surface_reference_parse
+  :: [ConcreteToken]
+  -> Prelude.Maybe ([ConcreteToken], DerivationResult)
+phase1_surface_reference_parse _ = Prelude.Nothing
 
 phase1_surface_reference_accepts :: [ConcreteToken] -> Prelude.Bool
 phase1_surface_reference_accepts _ = Prelude.False
