91b63ec6c19cc725105057626079a0246c9637af
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-11 13:45:30 +0200

    Export derivation types for source header refinement

--- proof/Phil/Surface/GrammarAstSourceSpine.v
+++ proof/Phil/Surface/GrammarAstSourceSpine.v
@@ -1,8 +1,8 @@
 From Stdlib Require Import Lists.List Strings.String.
 
+From Phil.Surface Require Export GrammarDerivation.
 From Phil.Surface Require Import
   Grammar
-  GrammarDerivation
   GrammarDerivationOracle
   GrammarParserRecognizer
   GrammarParserProgress
