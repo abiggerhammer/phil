3e10c24a7a9f2eb1e83fad0f655137e9252f2215
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-12 18:18:30 +0200

    Import source-header AST helpers directly

--- proof/Phil/Surface/GrammarAstTopLevelTotality.v
+++ proof/Phil/Surface/GrammarAstTopLevelTotality.v
@@ -1,6 +1,7 @@
 From Stdlib Require Import Lists.List Strings.String.
 
 From Phil.Surface Require Import
+  GrammarAstSourceHeader
   GrammarAstSourceHeaderTotality
   GrammarAstTopLevelSpine
   GrammarDeterminacySimpleResolverSoundness.
