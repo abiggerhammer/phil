fbed1b3928e537d145779c55a362336f04b9885f
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-12 06:35:12 +0200

    Export recognizer soundness through production kernel

--- proof/Phil/Surface/GrammarParserProductionKernel.v
+++ proof/Phil/Surface/GrammarParserProductionKernel.v
@@ -1,10 +1,11 @@
 From Stdlib Require Import Lists.List.
 
-From Phil.Surface Require Export GrammarDerivationOracle.
+From Phil.Surface Require Export
+  GrammarDerivationOracle
+  GrammarParserRecognizer.
 From Phil.Surface Require Import
   GrammarDerivation
-  GrammarParserProgress
-  GrammarParserRecognizer.
+  GrammarParserProgress.
 
 Import ListNotations.
 
