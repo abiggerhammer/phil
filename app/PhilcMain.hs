e282ad8fd434f92cfa02691852dfabdffb46ac45
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-12 03:37:53 +0200

    INT-011: require explicit target in philc

--- app/PhilcMain.hs
+++ app/PhilcMain.hs
@@ -6,7 +6,10 @@ import qualified Data.Text as Text
 import qualified Data.Text.IO as TextIO
 import Phil.Compiler
   ( RunnableProgram (runnableLLVMArtifact)
-  , compileRunnable
+  , compileRunnableForTarget
+  , parseCompilerTarget
+  , supportedCompilerTargets
+  , compilerTargetName
   , renderRunnableCompileError
   )
 import Phil.LLVM (llvmArtifactText)
@@ -18,15 +21,24 @@ main :: IO ()
 main = do
   arguments <- getArgs
   case arguments of
-    ["emit-llvm", path] -> emitLLVM path
-    _ -> do
-      TextIO.hPutStrLn stderr "usage: philc emit-llvm FILE"
-      exitFailure
+    ["emit-llvm", "--target", targetName, path] ->
+      case parseCompilerTarget (Text.pack targetName) of
+        Nothing -> do
+          TextIO.hPutStrLn stderr ("unknown target: " <> Text.pack targetName)
+          usage
+        Just target -> emitLLVM target path
+    _ -> usage
 
-emitLLVM :: FilePath -> IO ()
-emitLLVM path = do
+usage :: IO a
+usage = do
+  TextIO.hPutStrLn stderr "usage: philc emit-llvm --target TARGET FILE"
+  TextIO.hPutStrLn stderr
+    ("supported targets: " <> Text.intercalate ", " (map compilerTargetName supportedCompilerTargets))
+  exitFailure
+
+emitLLVM target path = do
   source <- TextIO.readFile path
-  case compileRunnable (Text.pack path) source of
+  case compileRunnableForTarget target (Text.pack path) source of
     Left compileError -> do
       TextIO.hPutStrLn stderr (renderRunnableCompileError compileError)
       exitFailure
