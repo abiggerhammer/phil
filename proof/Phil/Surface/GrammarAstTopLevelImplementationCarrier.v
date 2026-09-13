6dff3a27398c1995d1ce3b165f77d797e90a9eb6
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-13 11:21:13 +0200

    Import top-level declaration tag directly

--- proof/Phil/Surface/GrammarAstTopLevelImplementationCarrier.v
+++ proof/Phil/Surface/GrammarAstTopLevelImplementationCarrier.v
@@ -1,7 +1,8 @@
 From Stdlib Require Import Lists.List Strings.String.
 
 From Phil.Surface Require Import
-  GrammarAstTopLevelImplementationRepresentation.
+  GrammarAstTopLevelImplementationRepresentation
+  GrammarAstTopLevelSpine.
 
 Import ListNotations.
 
