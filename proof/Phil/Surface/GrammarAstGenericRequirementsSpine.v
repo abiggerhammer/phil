ddaafe7bda73812c59b694e6f4a25ff4459366a9
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-14 07:15:57 +0200

    Import surface AST destructors directly

--- proof/Phil/Surface/GrammarAstGenericRequirementsSpine.v
+++ proof/Phil/Surface/GrammarAstGenericRequirementsSpine.v
@@ -2,6 +2,7 @@ From Stdlib Require Import Lists.List Strings.String.
 
 From Phil.Surface Require Import
   GrammarDerivation
+  GrammarAstSourceHeader
   GrammarAstStructuralModeSpine.
 
 Import ListNotations.
