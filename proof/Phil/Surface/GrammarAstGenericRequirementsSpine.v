86b54c6fcd1fd4bc4f6400dde0ad8f52070fe6dd
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-14 08:33:54 +0200

    Remove redundant refined substitution

--- proof/Phil/Surface/GrammarAstGenericRequirementsSpine.v
+++ proof/Phil/Surface/GrammarAstGenericRequirementsSpine.v
@@ -546,7 +546,7 @@ Proof.
   destruct
     (phase1_surface_normalize_optional_generic_requirements requirements_tree)
     as [requirements |] eqn:Hrequirements; try discriminate Hnormalize.
-  inversion Hnormalize; subst refined.
+  inversion Hnormalize.
   unfold phase1_surface_record_requirements_spine_tree,
     phase1_surface_record_mode_spine_tree.
   cbn.
