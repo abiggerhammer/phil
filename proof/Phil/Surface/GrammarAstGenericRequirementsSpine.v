c2dcfd81e78c777101beeb4cf8f4a80bb5c01526
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-14 08:44:24 +0200

    Make record requirements equality explicit

--- proof/Phil/Surface/GrammarAstGenericRequirementsSpine.v
+++ proof/Phil/Surface/GrammarAstGenericRequirementsSpine.v
@@ -546,7 +546,8 @@ Proof.
   destruct
     (phase1_surface_normalize_optional_generic_requirements requirements_tree)
     as [requirements |] eqn:Hrequirements; try discriminate Hnormalize.
-  inversion Hnormalize.
+  injection Hnormalize as Hrefined.
+  rewrite <- Hrefined.
   unfold phase1_surface_record_requirements_spine_tree,
     phase1_surface_record_mode_spine_tree.
   cbn.
