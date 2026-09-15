0a88f4891d7ea582897fbbb8aa130ffb44dcd369
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-15 10:37:37 +0200

    proof: import variant carrier dependencies directly

--- proof/Phil/Surface/GrammarAstVariantSpine.v
+++ proof/Phil/Surface/GrammarAstVariantSpine.v
@@ -3,6 +3,8 @@ From Stdlib Require Import Lists.List Strings.String.
 From Phil.Surface Require Import
   GrammarDerivation
   GrammarAstSourceHeader
+  GrammarAstGenericParamsSpine
+  GrammarAstStructuralModeSpine
   GrammarAstDataSpine
   GrammarAstGenericRequirementsSpine.
 
