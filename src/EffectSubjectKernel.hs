d1b3c4f0e4f2f802fb874ec30c8d9025602abb5a
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-16 08:05:50 +0200

    Check in extracted effect subject kernel

---
+++ src/EffectSubjectKernel.hs
@@ -0,0 +1,52 @@
+module EffectSubjectKernel where
+
+import qualified Prelude
+
+data EffectSubjectCorrespondenceDecision =
+   EffectSubjectCorrespondenceAccepted
+ | EffectSubjectCorrespondenceSourceEmpty
+ | EffectSubjectCorrespondenceTargetEmpty
+ | EffectSubjectCorrespondenceRevisionEmpty
+
+decideEffectSubjectCorrespondence :: Prelude.Bool -> Prelude.Bool ->
+                                     Prelude.Bool ->
+                                     EffectSubjectCorrespondenceDecision
+decideEffectSubjectCorrespondence sourceNonempty targetNonempty revisionNonempty =
+  case sourceNonempty of {
+   Prelude.True ->
+    case targetNonempty of {
+     Prelude.True ->
+      case revisionNonempty of {
+       Prelude.True -> EffectSubjectCorrespondenceAccepted;
+       Prelude.False -> EffectSubjectCorrespondenceRevisionEmpty};
+     Prelude.False -> EffectSubjectCorrespondenceTargetEmpty};
+   Prelude.False -> EffectSubjectCorrespondenceSourceEmpty}
+
+data EffectSubjectRetargetDecision =
+   EffectSubjectRetargetAcceptedSame
+ | EffectSubjectRetargetAcceptedCorrespondence
+ | EffectSubjectRetargetIndexOutOfRange
+ | EffectSubjectRetargetRequiresCorrespondence
+ | EffectSubjectRetargetCorrespondenceSourceMismatch
+ | EffectSubjectRetargetCorrespondenceTargetMismatch
+
+decideEffectSubjectRetarget :: Prelude.Bool -> Prelude.Bool -> Prelude.Bool ->
+                               Prelude.Bool -> Prelude.Bool ->
+                               EffectSubjectRetargetDecision
+decideEffectSubjectRetarget indexInRange sameSubject correspondencePresent correspondenceSourceMatches correspondenceTargetMatches =
+  case indexInRange of {
+   Prelude.True ->
+    case sameSubject of {
+     Prelude.True -> EffectSubjectRetargetAcceptedSame;
+     Prelude.False ->
+      case correspondencePresent of {
+       Prelude.True ->
+        case correspondenceSourceMatches of {
+         Prelude.True ->
+          case correspondenceTargetMatches of {
+           Prelude.True -> EffectSubjectRetargetAcceptedCorrespondence;
+           Prelude.False -> EffectSubjectRetargetCorrespondenceTargetMismatch};
+         Prelude.False -> EffectSubjectRetargetCorrespondenceSourceMismatch};
+       Prelude.False -> EffectSubjectRetargetRequiresCorrespondence}};
+   Prelude.False -> EffectSubjectRetargetIndexOutOfRange}
+
