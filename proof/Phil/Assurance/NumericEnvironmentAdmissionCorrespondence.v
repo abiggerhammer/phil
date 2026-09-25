ef73f25507758492c0fde367f823a3f4c1794aa9
Meredith L. Patterson <clonearmy@gmail.com>
2026-09-25 08:07:35 +0200

    Model numeric environment source tuple

--- proof/Phil/Assurance/NumericEnvironmentAdmissionCorrespondence.v
+++ proof/Phil/Assurance/NumericEnvironmentAdmissionCorrespondence.v
@@ -1,3 +1,16 @@
+Definition NumericLeafId := nat.
+Definition NumericDeclarationId := nat.
+Definition NumericScopeId := nat.
+Definition NumericDescriptor := nat.
+
+Record NumericEnvironmentAdmissionModel : Type :=
+  mkNumericEnvironmentAdmissionModel {
+    modelEvaluatorLeaf : NumericLeafId -> bool;
+    modelActualDeclaration : NumericLeafId -> option NumericDeclarationId;
+    modelActualScope : NumericLeafId -> option NumericScopeId;
+    modelActualDescriptor : NumericLeafId -> option NumericDescriptor
+  }.
+
 Theorem numeric_environment_admission_boundary_is_explicit :
   forall P : Prop, P -> P.
 Proof.
